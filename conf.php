645039da07a525142b5539aef94dd17891308cef
al1can <93518653+al1can@users.noreply.github.com>
2021-11-01 17:09:49 +0300

    mysql connection problems

--- conf.php
+++ conf.php
@@ -1,12 +1,18 @@
 <?php
 $servername = 'localhost';
-$username = 'root';
-$password = '5511114131Ag';
+$username = 'al1can';
+$password = '';
 $dbname = 'loginpage';
 
-$conn = mysql_connect($servername,$username,$password,$dbname);
-if (!conn) {
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+if (!$conn) {
 	die("Connection failed! : ". mysqli_connect_error());
 }
 echo "Connection succesfull";
+$sql = 'select * from users';
+$result = mysqli_query($conn, $sql);
+while ($row = mysqli_fetch_assoc($result)) {
+	echo $row['firstname'];
+}
 ?>
